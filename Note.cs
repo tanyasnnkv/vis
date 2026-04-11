0d35c92b4c919fbd4979b17c7c5f2891c544bab1
tanyasnnkv <sannikova-tatyana23@yandex.ru>
2026-04-11 19:30:55 +0300

    Lab4

---
+++ Note.cs
@@ -0,0 +1,10 @@
+﻿namespace IsLabApp
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+using IsLabApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+var notes = new List<Note>();
+var nextId = 1;
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/health", () =>
+{
+    return Results.Json(new
+    {
+        status = "ok",
+        time = DateTime.UtcNow
+    });
+});
+
+app.MapGet("/version", (IConfiguration config) =>
+{
+    var name = config["App:Name"];
+    var version = config["App:Version"];
+
+    return Results.Json(new
+    {
+        application = name,
+        version = version
+    });
+});
+
+app.MapGet("/api/notes", () =>
+{
+    return notes;
+});
+
+app.MapGet("/api/notes/{id}", (int id) =>
+{
+    var note = notes.FirstOrDefault(n => n.Id == id);
+    return note is not null ? Results.Ok(note) : Results.NotFound();
+});
+
+app.MapPost("/api/notes", (Note note) =>
+{
+    note.Id = nextId++;
+    note.CreatedAt = DateTime.UtcNow;
+    notes.Add(note);
+    return Results.Created($"/api/notes/{note.Id}", note);
+});
+
+app.MapDelete("/api/notes/{id}", (int id) =>
+{
+    var note = notes.FirstOrDefault(n => n.Id == id);
+    if (note is null)
+        return Results.NotFound();
+
+    notes.Remove(note);
+    return Results.Ok();
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
